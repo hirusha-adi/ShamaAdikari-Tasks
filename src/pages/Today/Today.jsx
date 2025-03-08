38098390de68bd1ee0922108d3f15e53ade21d56
hirusha-adi <hirushaadi@gmail.com>
2025-03-08 21:05:40 +0530

    change

--- src/pages/Today/Today.jsx
+++ src/pages/Today/Today.jsx
@@ -8,10 +8,21 @@ const Today = () => {
 
   return (
     <>
-      <div className="flex justify-center items-center">
-        <h1>උසාවි දින පොත</h1>
+      <div className="min-h-screen">
+        <div className="pt-8">
+
+          <div className="flex justify-center items-center">
+
+            <div className="tabs tabs-box">
+              <input type="radio" name="my_tabs_1" className="tab" aria-label={`Today ${new Date().getDate()} [${new Date().toLocaleString('default', { weekday: 'long' })}]`} defaultChecked />
+              <input type="radio" name="my_tabs_1" className="tab" aria-label={`Tomorrow ${new Date(Date.now() + 1000 * 60 * 60 * 24).getDate()} [${new Date(Date.now() + 1000 * 60 * 60 * 24).toLocaleString('default', { weekday: 'long' })}]`} />
+            </div>
+
+          </div>
+
+
+        </div>
       </div>
-      <div className="btn">Button</div>
     </>
   );
 };
