b8581c1ca844630f76bd6b5183aef2b55b834e92
hirusha-adi <hirushaadi@gmail.com>
2025-03-08 13:52:22 +0530

    for toastify

---
+++ src/utils/consts.js
@@ -0,0 +1,12 @@
+
+// for `react-toastify`
+export const toastConfig = {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: false,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+}
