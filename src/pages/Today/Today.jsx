64cef3cc7d3da90deb062828937a03b6ba191692
hirusha-adi <hirushaadi@gmail.com>
2025-03-08 21:09:21 +0530

    show tabs

--- src/pages/Today/Today.jsx
+++ src/pages/Today/Today.jsx
@@ -1,30 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Today = () => {
+  const [selectedTab, setSelectedTab] = useState("today");
 
   useEffect(() => {
-    document.title = `Home`
-  })
+    document.title = selectedTab === "today" ? "Today" : "Tomorrow";
+  }, [selectedTab]);
 
   return (
-    <>
-      <div className="min-h-screen">
-        <div className="pt-8">
-
-          <div className="flex justify-center items-center">
-
-            <div className="tabs tabs-box">
-              <input type="radio" name="my_tabs_1" className="tab" aria-label={`Today ${new Date().getDate()} [${new Date().toLocaleString('default', { weekday: 'long' })}]`} defaultChecked />
-              <input type="radio" name="my_tabs_1" className="tab" aria-label={`Tomorrow ${new Date(Date.now() + 1000 * 60 * 60 * 24).getDate()} [${new Date(Date.now() + 1000 * 60 * 60 * 24).toLocaleString('default', { weekday: 'long' })}]`} />
-            </div>
-
+    <div className="min-h-screen">
+      <div className="pt-8">
+        <div className="flex justify-center items-center">
+          <div className="tabs tabs-box">
+            <input
+              type="radio" name="dateSelection" value="today" className="tab"
+              aria-label={`Today ${new Date().getDate()} [${new Date().toLocaleString('default', { weekday: 'long' })}]`}
+              checked={selectedTab === "today"}
+              onChange={() => setSelectedTab("today")}
+            />
+            <input
+              type="radio" name="dateSelection" value="tomorrow" className="tab"
+              aria-label={`Tomorrow ${new Date(Date.now() + 1000 * 60 * 60 * 24).getDate()} [${new Date(Date.now() + 1000 * 60 * 60 * 24).toLocaleString('default', { weekday: 'long' })}]`}
+              checked={selectedTab === "tomorrow"}
+              onChange={() => setSelectedTab("tomorrow")}
+            />
           </div>
-
-
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
-export { Today }
+export { Today };
