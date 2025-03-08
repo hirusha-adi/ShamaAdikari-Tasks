603caf1b327ef70dfe7207ad34d742f42fe8a5a6
hirusha-adi <hirushaadi@gmail.com>
2025-03-08 15:30:33 +0530

    pick date

--- src/pages/New/New.jsx
+++ src/pages/New/New.jsx
@@ -1,17 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/style.css";
 
 const New = () => {
+
+  const [naduDetails, setNaduDetails] = useState("");
+  const [naduCaseNumber, setNaduCaseNumber] = useState("");
+  const [naduDate, setNaduDate] = useState(null);
 
   useEffect(() => {
     document.title = `New`
   })
 
+  async function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <>
-      <div className="flex justify-center items-center">
-        <h1>New</h1>
+      <div className="bg-gray-100">
+        <div className="container mx-auto min-h-screen">
+          <button popovertarget="rdp-popover" className="input input-border" style={{ anchorName: "--rdp" }}>
+            {naduDate ? naduDate.toLocaleDateString() : "Pick a date"}
+          </button>
+          <div popover="auto" id="rdp-popover" className="dropdown" style={{ positionAnchor: "--rdp" }}>
+            <DayPicker className="react-day-picker" mode="single" selected={naduDate} onSelect={setNaduDate} />
+          </div>
+
+        </div>
       </div>
-      <div className="btn">Button</div>
     </>
   );
 };
